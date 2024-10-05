0b1fd0a4b7cfd60982848cd4e4e6bc065f04ef3f
Chhoti Mishra <137808761+chhotimishra13@users.noreply.github.com>
2024-10-05 10:40:00 +0000

    Data Types

--- basics/datatype.js
+++ basics/datatype.js
@@ -2,3 +2,22 @@
 
 //alert("hello");  //we are using node.js, not browser
 
+let name = "Chhoti";
+let age = 18;
+let isBoolean = true;
+
+// primitive data types
+//number -->  2 to power 53
+//bigint 
+//string --> " "
+//boolean --> true or false
+//null --> it is standalone value
+//undefained --> when value is not assign
+//symbol --> it is used when we find a uniqueness
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
