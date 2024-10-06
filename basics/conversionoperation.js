bb2536715bb73f595f9b367a46a991e3f5eae132
Chhoti Mishra <137808761+chhotimishra13@users.noreply.github.com>
2024-10-06 06:44:03 +0000

    Conversion Operation

--- basics/conversionoperation.js
+++ basics/conversionoperation.js
@@ -1,25 +1,74 @@
-let score = "33abc";
-let value = true;
-console.log(typeof score);
+// let score = "33abc";
+// let value = true;
+// console.log(typeof score);
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-//"33" --> 33
-//"33abc" --> NaN
-//TRUE --> 1 ; FALSE -->0
+// //"33" --> 33
+// //"33abc" --> NaN
+// //TRUE --> 1 ; FALSE -->0
 
-let isLoggedIn = "";
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = "";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-//""  --> false
-//1 --> true; 0 --> false
-//"chhoti" --> true
+// //""  --> false
+// //1 --> true; 0 --> false
+// //"chhoti" --> true
 
 
-let someNumber = 22;
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 22;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ****************************** Operation *********************************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " javascript";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" +2);
+console.log(1 + "2");
+console.log("1"  + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "3");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+let gameCounter1 = 100;
+++gameCounter1;
+console.log(gameCounter1);
+
+//link to study
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+let x = 3;
+let y = x++;
+console.log(`x: ${x}, y: ${y}`);
+
+let a = 3;
+const b = ++a;
+
+console.log(`a: ${a}, b: ${b}`);
