f5782bee760898e5ecd80772f395172e44bcf3c2
Chhoti Mishra <137808761+chhotimishra13@users.noreply.github.com>
2024-10-06 07:17:45 +0000

    Summary of Data Types

---
+++ basics/datatypes_summary.js
@@ -0,0 +1,39 @@
+// Primitive  ------ call by value
+
+// 7 types: String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100;
+const scoreValue  = 100.3
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail  = undefined;
+
+const id = Symbol('123');
+
+const bigInt = 34456786424556776n;
+console.log(bigInt);
+
+
+
+
+// Reference tpye or Non Primitive
+//return types of non primitive data types is always object/function
+//Array, Objects, Functions
+const heros = ["shakitman","naagraj","doga"];
+
+//object
+let myObj = {
+    name: "chhoti",
+    age: 21,
+}
+
+//function
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+//to check the datatypes then use
+console.log(typeof outsideTemp);
+
+// JavaScript is dynamically typed, meaning that type checking happens at runtime, not during compilation
