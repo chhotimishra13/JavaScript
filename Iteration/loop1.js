452aadce2f955292a59073076befb9e94d03e182
Chhoti Mishra <137808761+chhotimishra13@users.noreply.github.com>
2024-10-24 05:35:31 +0000

    while & do while loop

---
+++ Iteration/loop1.js
@@ -0,0 +1,23 @@
+//while & do while loop
+
+let i = 0;
+while (i <= 10) {
+    console.log(`value of i is ${i}`);
+    i++;
+}
+
+
+let arr = [1,2,3,4,6];
+let j = 0;
+while(j < arr.length){
+    console.log(`value is ${arr[j]}`);
+    j++;
+}
+
+//do while
+
+let score = 11;
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <= 10);
