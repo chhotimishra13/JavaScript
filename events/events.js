1876e3e154f358ab95d351a9d5c7c470cc2c60f5
Chhoti Mishra <137808761+chhotimishra13@users.noreply.github.com>
2024-10-27 08:36:33 +0000

    Async javascript

---
+++ events/events.js
@@ -0,0 +1,22 @@
+//javascript
+//.. synchronous
+//.. single threaded
+
+//execution context
+//.. execute one line of code at a time
+//.. |console.log -> 1
+//.. |console log -> 2
+//   |
+
+
+//Blocking code vs Non - Blocking code
+
+//.. Blocking code ----
+//... block the flow of program
+//... read file sync
+
+//Non - blocking code
+//... Does not block execution
+//... read file async
+
+//javascript engine - Memory heap + call stack
